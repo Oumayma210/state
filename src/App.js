d7c4c358494a5a39fcae3d930924ab206b9919b0
Oumayma210 <abdellioumayma210@gmail.com>
2021-08-29 01:22:47 +0100

    stateck

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Card} from 'react-bootstrap'
+class App extends Component {
+  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  state={
+    person: {
+    fullName:"Oumayma Abdelli",
+    bio:"Les gagnants trouvent des moyens, les perdants des excuses",
+    imgSrc:"./ouou.jpg",
+    profession:"Full stack Js developer",
+  },show:false,
+    time: 0,
+};
+componentDidMount() {
+  this.myInterval = setInterval(() => {
+    this.setState({
+      time: this.state.time + 1,
+    });
+  }, 1000);
 }
+componentWillUnmount(){
+  clearInterval(setInterval)
+}
+  render() {
+    return (
+      
+  <div className='App'>
 
+      {this.state.show === true ? ( 
+        <Card style={{ width: '16rem', height:'25rem', backgroundColor:'burlywood', justifyContent: 'center' }}>
+        <Card.Img variant="top" src={this.state.person.imgSrc} alt='' height='200px' width='80px' style={{borderRadius:'200px', border:'1px blue solid'}} />
+         <Card.Body>
+         <Card.Title className='titre' style={{color:'brown',fontSize:'25px',fontFamily: 'Times New Roman'}}>{this.state.person.fullName}</Card.Title>
+        <Card.Text className='bioo'style={{fontSize:'16px',fontFamily: 'Times New Roman'}}>
+        {this.state.person.bio}
+       </Card.Text>
+   <Card.Text className='prof' style={{fontSize:'16px',fontFamily: 'Times New Roman'}}>
+     {this.state.person.profession}
+   </Card.Text>
+ </Card.Body>
+ <div >
+          <p style={{fontSize:'14px',fontFamily: 'Times New Roman'}}> Count: {this.state.time}</p>
+        </div>
+ </Card> 
+
+      ): (
+        " "
+      )}
+         
+         
+         <div>
+           <button 
+            className='btnn' style={{color:'white', borderRadius:'30px', backgroundColor:'blue', justifyContent:'center'}}
+            onClick={() => this.setState({ show: !this.show })}>ClickMe
+
+
+           </button>
+         </div>
+</div>
+        );
+  }
+}
 export default App;
